3d64a82e79c6efac3d54d33a0009b4288c24b8fe
Aliyan Chanegaon <111250756+AliyanChanegaon@users.noreply.github.com>
2023-08-26 15:55:32 +0530

    Update flavanoids.tsx

--- my-app/src/components/flavanoids.tsx
+++ my-app/src/components/flavanoids.tsx
@@ -3,15 +3,16 @@ import { ResultModelProp } from "../models/resultModel";
 import {
   calculateMean,
   calculateMedian,
-  calculateMode,
+  calculateMode
 } from "../utils/util-fuctions";
+import { Table } from "./table";
 
 const Flavanoids = ({ data }: { data: DataItemsProp[] }) => {
   const classes: Record<string, number[]> = {};
 
   data.forEach((item) => {
     const classNo = item.Alcohol;
-    
+
     if (!classes[classNo]) {
       classes[classNo] = [];
     }
@@ -19,52 +20,21 @@ const Flavanoids = ({ data }: { data: DataItemsProp[] }) => {
     classes[classNo].push(+item.Flavanoids);
   });
 
-
   const result: ResultModelProp = {};
   for (const classNum in classes) {
     const classData = classes[classNum];
 
-
     result[classNum] = {
       Mean: calculateMean(classData),
       Median: calculateMedian(classData),
-       Mode: calculateMode(classData),
+      Mode: calculateMode(classData)
     };
   }
 
   return (
     <div>
       <h2>Flavanoids</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Measure</th>
-            {Object.keys(result).map((classNum) => (
-              <th key={classNum}>Class {classNum}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Mean</td>
-            {Object.keys(result).map((classNum) => (
-              <td key={classNum}>{result[classNum].Mean.toFixed(3)}</td>
-            ))}
-          </tr>
-          <tr>
-            <td>Median</td>
-            {Object.keys(result).map((classNum) => (
-              <td key={classNum}>{result[classNum].Median.toFixed(3)}</td>
-            ))}
-          </tr>
-          <tr>
-            <td>Mode</td>
-            {Object.keys(result).map((classNum) => (
-              <td key={classNum}>{result[classNum]?.Mode?.toFixed(3)}</td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+      <Table data={result} />
     </div>
   );
 };
